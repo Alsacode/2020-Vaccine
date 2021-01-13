ed9e049ffbabf90a0c4a7a7c365f50f070ab3b38
Alsacode <alaa.salonen@gmail.com>
2021-01-14 00:17:41 +0200

    Components Added

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import styles from './app.module.scss';
+import Item from '../item';
+function App() {
+  return (
+    <div className={styles.app}>
+      <Item />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/item/item.js
@@ -0,0 +1,12 @@
+import styles from './item.module.scss';
+
+
+function Item(props) {
+    return (
+        <div className={styles.item}>
+            Covid-19
+        </div>
+    );
+}
+
+export default Item;
